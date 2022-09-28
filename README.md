13bf6deb07c0674b8e673ef1d1834026be393104
wilco <wilcologger@gmail.com>
2022-09-28 15:54:18 +0200

    add function to random chose a color

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Color-flipper
+
+In this project i'm going to make a simple color switch.
+The purpose of this project is te learn and understand JavaScript better.
+I'm not going over the HTML of the CSS of this project. This is pure JavaScript learning.
+
+Key concepts covered:
+- arrays
+- document.getElementById()
+- document.querySelector()
+- addEventListener()
+- document.body.style.backgroundColor
+- Math.floor()
+- Math.random()
+- array.length
+
+Here are the steps that a take to make this project.
+1. Make the button clickable.
+2. Make sure the color is active if the button is clicked.
+3. 
---
+++ script.js
@@ -0,0 +1,21 @@
+// Button ID
+const btn = document.getElementById("btn");
+// Array of colors values
+const colors = ["red", "green", "blue", "orange"];
+// Get color class
+const color = document.querySelector(".color");
+
+// Click function
+btn.addEventListener("click", () => {
+  console.log("Click!");
+  // Get random number between 0 - 3
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
